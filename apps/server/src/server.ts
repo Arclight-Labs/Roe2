428bf7fdb6438fe74960ee0058ed4f66d8eae01a
Jasper Concepcion <jasper@manok.dev>
2022-04-13 20:44:32 +0800

    fix(ws): cors allow everything

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -6,10 +6,15 @@ import { tournamentEvents } from "./events/tournaments"
 const PORT = process.env.PORT || 1337
 
 const httpServer = http.createServer()
-const io = new Server(httpServer)
+const io = new Server(httpServer, {
+  cors: { origin: "*" },
+})
 
 io.on("connection", async (socket) => {
   console.log(`New Connection: ${socket.id}`)
+  socket.onAny((eventName, ...args: any[]) => {
+    console.log(`[${socket.id}]: emitted ${eventName} with ${args}`)
+  })
 
   socket.on("ping", (data) => {
     io.emit("ping", data)
