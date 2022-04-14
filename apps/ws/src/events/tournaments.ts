0e01b88ebb9c3ce3624fbd4992ad12c34193d670
Jasper Concepcion <jasper@manok.dev>
2022-04-14 13:06:50 +0800

    fix(ws): tournament interface renamed

--- apps/ws/src/events/tournaments.ts
+++ apps/ws/src/events/tournaments.ts
@@ -1,9 +1,9 @@
 import { Server, Socket } from "socket.io"
-import { SocketEvent, Tournament } from "interface"
+import { SocketEvent, TournamentInterface } from "interface"
 import { setTournament } from "../store/tournament"
 
 export const tournamentEvents = (io: Server, socket: Socket) => {
-  socket.on(SocketEvent.Tournament, (payload: Partial<Tournament>) => {
+  socket.on(SocketEvent.Tournament, (payload: Partial<TournamentInterface>) => {
     const newTournament = setTournament(payload)
     io.emit("tournament", newTournament)
   })
