7a8f54ed6f7885dc9bc0e8df0c361a4b20a3b308
Jasper Concepcion <jasper@manok.dev>
2022-04-14 21:00:14 +0800

    fix(db): 🔒️ just add empty string no DEV_Secret

--- apps/api/src/utils/hash.ts
+++ apps/api/src/utils/hash.ts
@@ -2,6 +2,6 @@ import crypto from "crypto"
 
 export default function hash(value: string) {
   const algo = "sha512"
-  const secret = process.env.DEV_SECRET || "dev_secret"
+  const secret = process.env.DEV_SECRET || ""
   return crypto.createHmac(algo, secret).update(value).digest("hex")
 }
