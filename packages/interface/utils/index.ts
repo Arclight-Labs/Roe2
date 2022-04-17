e737cfdccf45dfa1ca473ffb279ed4300e9f40cc
Jasper Concepcion <jasper@manok.dev>
2022-04-18 04:40:44 +0800

    chore: rename WithAuth

--- packages/interface/utils/index.ts
+++ packages/interface/utils/index.ts
@@ -1,3 +1,4 @@
 export type { Path } from "./Path.interface"
 export type { NotifProps } from "./Notif.interface"
 export { Cookie } from "./Cookie.enum"
+export type { WithAuth } from "./WithAuth.interface"
