e332b19afcaf43ef08e7d5c03217e965c1914b0c
Jasper Concepcion <jasper@manok.dev>
2022-05-03 21:37:11 +0800

    fix: user schema error messages

--- packages/utils/schema/user.schema.ts
+++ packages/utils/schema/user.schema.ts
@@ -1,13 +1,15 @@
 import { z } from "zod"
 export const userCreateSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(6),
+  password: z.string().min(6, "Password must be at least 6 characters"),
 })
 export type UserCreateInfer = z.infer<typeof userCreateSchema>
 
 export const userUpdateSchema = z.object({
-  username: z.string().min(1),
-  socialHandle: z.string().min(3),
+  username: z.string().min(3, "Username must be at least 3 characters"),
+  socialHandle: z
+    .string()
+    .min(3, "Social Handle must be at least 3 characters"),
   avatar: z.string().optional(),
   isTalent: z.boolean(),
 })
