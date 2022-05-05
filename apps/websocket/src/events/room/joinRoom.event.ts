2e217e270df94a59e03ef6ef0f383d262ee62e59
Jasper Concepcion <jasper@manok.dev>
2022-05-05 19:43:12 +0800

    fix(ws): add roomId to room log message

--- apps/websocket/src/events/room/joinRoom.event.ts
+++ apps/websocket/src/events/room/joinRoom.event.ts
@@ -4,7 +4,7 @@ import { EventFn } from "../event.hoc"
 export const joinRoomEvent: EventFn<JoinRoom> = (socket, io) => {
   return async ({ roomId, roomName, username }) => {
     socket.join(roomId)
-    const log = `${username}:${socket.id} joined room ${roomName}`
+    const log = `[${username}:${socket.id}] joined room [${roomName}:${roomId}]`
     console.log(log)
     io.to(roomId).emit(SocketEvent.Log, log)
   }
--- apps/websocket/src/events/room/leaveRoom.event.ts
+++ apps/websocket/src/events/room/leaveRoom.event.ts
@@ -5,7 +5,7 @@ import { EventFn } from "../event.hoc"
 export const leaveRoomEvent: EventFn<LeaveRoom> = (socket, io) => {
   return async ({ roomId, roomName, username }) => {
     socket.leave(roomId)
-    const log = `${username}:${socket.id} left room ${roomName}`
+    const log = `[${username}:${socket.id}] left room [${roomName}:${roomId}]`
     console.log(log)
     io.to(roomId).emit(SocketEvent.Log, log)
   }
