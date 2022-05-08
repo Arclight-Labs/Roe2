310addfca63bd06805258cb0e8b50c29d7741259
Jasper Concepcion <jasper@manok.dev>
2022-05-09 06:14:56 +0800

    fix: add prod url to websocket url

--- packages/utils/socket/Socket.instance.ts
+++ packages/utils/socket/Socket.instance.ts
@@ -8,8 +8,11 @@ import {
   participantsListen,
 } from "./events"
 
+const isDev = import.meta.env.DEV
 const SOCKET_PORT = 1337
-const SOCKET_URL = `${window.location.hostname}:${SOCKET_PORT}`
+const DEV_URL = `${window.location.hostname}:${SOCKET_PORT}`
+const PROD_URL = `https://servers.acadarena.com/roe2`
+const SOCKET_URL = isDev ? DEV_URL : PROD_URL
 
 export const socket = io(SOCKET_URL, {
   reconnection: true,
