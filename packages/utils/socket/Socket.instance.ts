c836707dd5f66aa6799a2e28595dd95dea0c452d
Jasper Concepcion <jasper@manok.dev>
2022-05-09 09:54:03 +0800

    feat(ws): fix ws path and host

--- packages/utils/socket/Socket.instance.ts
+++ packages/utils/socket/Socket.instance.ts
@@ -11,10 +11,15 @@ import {
 const isDev = import.meta.env.DEV
 const SOCKET_PORT = 1337
 const DEV_URL = `${window.location.hostname}:${SOCKET_PORT}`
-const PROD_URL = `https://servers.acadarena.com/roe2`
+const PROD_URL = `https://servers.acadarena.com`
 const SOCKET_URL = isDev ? DEV_URL : PROD_URL
-
+const PATH = isDev ? "/socket.io" : "/roe2/socket.io"
+const config = {
+  host: SOCKET_URL,
+  path: PATH,
+}
 export const socket = io(SOCKET_URL, {
+  ...config,
   reconnection: true,
   extraHeaders: {
     cookie: "testasdad",
