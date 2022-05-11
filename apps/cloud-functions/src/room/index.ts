fc728bfaa3e49c961e94ff2162b6097fdc0306ed
Jasper Concepcion <jasper@manok.dev>
2022-05-11 18:35:05 +0800

    feat(cf): ✨ add default broadcast data when room is created

---
+++ apps/cloud-functions/src/room/index.ts
@@ -0,0 +1,1 @@
+export { onCreate } from "./onCreate.trigger"
---
+++ apps/cloud-functions/src/room/onCreate.trigger.ts
@@ -0,0 +1,21 @@
+import { Broadcast } from "interface/ws"
+import { functions } from "../admin"
+
+export const onCreate = functions.firestore
+  .document("rooms/{roomId}")
+  .onCreate(async (snap) => {
+    const ref = snap.ref
+
+    const liveRef = ref.collection("live").doc("broadcast")
+    return liveRef.set({
+      matches: {},
+      participants: {},
+      talents: {},
+      tournament: null,
+      roomId: snap.id,
+      activeMatch: "",
+      nextMatch: "",
+      prevMatches: [],
+      schedule: {},
+    } as Broadcast)
+  })
