29e9c65e1f505a6eede170c6a3445ed10c2ac842
Jasper Concepcion <onetruemanok@gmail.com>
2022-05-13 16:48:02 +0800

    fix: vite config allow svgs

--- apps/overlay/vite.config.ts
+++ apps/overlay/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import loadVersion from "vite-plugin-package-version"
+import svgrPlugin from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: { port: 1339 },
+  plugins: [
+    react(),
+    loadVersion(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
+  server: { port: 1338 },
 })
