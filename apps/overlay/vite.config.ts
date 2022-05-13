c41027950569d986f98d40477cb50a25facfe2e7
Jasper Concepcion <onetruemanok@gmail.com>
2022-05-13 17:00:29 +0800

    fix: package.json remove packageVersion

--- apps/overlay/vite.config.ts
+++ apps/overlay/vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import loadVersion from "vite-plugin-package-version"
 import svgrPlugin from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    loadVersion(),
     svgrPlugin({
       svgrOptions: {
         icon: true,
