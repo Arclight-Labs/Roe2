5a4ff4a3bc6605c41f71ccdaad71429f86ec39ce
Jasper Concepcion <jasper@manok.dev>
2022-05-16 13:19:31 +0800

    chore: prepare Match types

--- packages/interface/ws/Live.interface.ts
+++ packages/interface/ws/Live.interface.ts
@@ -1,7 +1,5 @@
 import { User } from "../db"
 
-export type Schedule = { matchId: string; date: Date }
-export type Lowerthird = {}
 export interface Live {
   activeMatch: string
   prevMatches: string[]
@@ -10,4 +8,84 @@ export interface Live {
   invert: boolean
   talents: Record<string, User>
   lt: Lowerthird
+}
+
+/**
+ * Adjustments
+ *
+ * 0 means it's disabled
+ */
+export interface AdjSize {
+  h: number
+  w: number
+  x: number
+  y: number
+  scale: number
+}
+
+export interface AdjText {
+  /*
+	0 = disabled
+   */
+  size: number
+  text: string
+}
+
+export interface Schedule {
+  matchId: string
+  date: Date
+}
+
+// ======== Lowerthird
+export interface Lowerthird {
+  show: boolean
+  data: LowerthirdData
+  mode: LowerthirdMode
+}
+
+export interface LowerthirdData {
+  ticker: Ticker
+  ad: Ad
+  adPool: AdPool
+  matchPoll: MatchPoll
+}
+
+type LowerthirdMode = keyof LowerthirdData
+
+export interface Ticker {
+  headline: AdjText
+  scrollerText: AdjText
+}
+
+export interface Ad {
+  headline: AdjText
+  body: AdjText
+  adj: AdjSize
+}
+
+export interface AdWithPriority extends Ad {
+  priority?: number
+}
+
+export interface AdPool {
+  ads: AdWithPriority[]
+  transitionDuration: number
+  duration: number
+}
+
+export interface MatchPollItem {
+  teamId: string
+  vote: number
+}
+
+export interface MatchPoll {
+  a: MatchPollItem
+  b: MatchPollItem
+}
+
+export interface Shoutout {
+  alias: string
+  message: string
+  image: string
+  avatar: string
 }
