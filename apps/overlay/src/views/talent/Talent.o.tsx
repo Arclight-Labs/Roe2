b513a24f1203f615d36e788b8a1df7f26d17bad2
AASonar <bryan@acadarena.com>
2022-05-20 19:13:07 +0800

    style(overlay): :sparkles: added custom Align

--- apps/overlay/src/views/talent/Talent.o.tsx
+++ apps/overlay/src/views/talent/Talent.o.tsx
@@ -2,23 +2,37 @@ import { useLive, useMatches, useParticipants } from "utils/hooks"
 import { useParams } from "react-router-dom"
 import useRoom from "../../hooks/useRoom.hook"
 import { Image, Box, Text } from "@mantine/core"
+import { useQuery } from "../../utils/useQuery"
 
 type Params = Record<"talent", string>
+type alignType = "left" | "center" | "right" | undefined
 
 const Talent = () => {
   useRoom()
   const params = useParams<Params>()
-
+  const query = useQuery()
   const { getActiveTalent } = useLive()
   const talentIndex = +(params.talent ?? 0)
   const talent = getActiveTalent(talentIndex)
+  const align = query.get("align") ?? "left"
 
   return (
-    <Box sx={{ height: 600, width: 600 }}>
-      <Text sx={{ fontFamily: "Industry", fontSize: 120 }}>
+    <Box
+      sx={{
+        height: 600,
+        width: 1000,
+      }}
+    >
+      <Text
+        sx={{ fontFamily: "Industry", fontSize: 120 }}
+        align={align as alignType}
+      >
         {talent?.username}
       </Text>
-      <Text sx={{ fontFamily: "Roboto", fontSize: 70 }}>
+      <Text
+        sx={{ fontFamily: "Roboto", fontSize: 70 }}
+        align={align as alignType}
+      >
         @{talent?.socialHandle}
       </Text>
     </Box>
