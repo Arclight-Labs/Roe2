af0d2b74b69e1f89acd171bb5d64c9e31235d774
Jasper Concepcion <jasper@manok.dev>
2022-05-28 18:21:38 +0800

    feat(ctrl): add hotkey for lt mode

--- apps/control/src/ui/lowerthirds/General.lt.ui.tsx
+++ apps/control/src/ui/lowerthirds/General.lt.ui.tsx
@@ -1,16 +1,16 @@
-import {
-  Radio,
-  RadioGroup,
-  RadioGroupProps,
-  Stack,
-  Switch,
-} from "@mantine/core"
+import { Group, GroupProps, Stack, StackProps, Switch } from "@mantine/core"
 import { Live, LowerthirdData } from "interface/ws"
 import { useLt } from "utils/hooks"
 import { useBSave } from "../../context/bsave/bsave.hook"
 import { setLive } from "utils/socket/events"
+import LowerthirdModeCard from "./ModeCard.lt.ui"
+import { FC } from "react"
+import { HotkeyItem, useHotkeys } from "@mantine/hooks"
 
-const LowerthirdGeneral = () => {
+interface Props extends StackProps {
+  groupProps?: GroupProps
+}
+const LowerthirdGeneral: FC<Props> = ({ groupProps, ...props }) => {
   const { mode, show, data } = useLt()
   const bSave = useBSave()
 
@@ -22,25 +22,42 @@ const LowerthirdGeneral = () => {
     bSave(toggleLT)
   }
 
-  const onSelect: RadioGroupProps["onChange"] = (value) => {
-    const saveData: Partial<Live> = {
-      lt: { mode: (value as keyof LowerthirdData) || "ticker", data, show },
-    }
-    setLive(saveData)
-    bSave(saveData)
-  }
+  const modes = ["ticker", "ad", "adPool", "matchPoll"]
+
+  useHotkeys([
+    ["mod+O", toggleLowerThirds],
+    ...modes.map<HotkeyItem>((m, i) => {
+      return [
+        `mod+${i + 1}`,
+        () => {
+          const newData: Partial<Live> = {
+            lt: {
+              data,
+              show: mode === m ? !show : true,
+              mode: m as keyof LowerthirdData,
+            },
+          }
+          setLive(newData)
+          bSave(newData)
+        },
+      ]
+    }),
+  ])
 
   return (
-    <Stack>
+    <Stack {...props}>
       <Switch
         label="Toggle Lower Thirds"
         checked={show}
         onChange={toggleLowerThirds}
       />
 
-      <RadioGroup value={mode} onChange={onSelect}>
-        <Radio value="react" label="React" />
-      </RadioGroup>
+      <Group {...groupProps}>
+        <LowerthirdModeCard value={"ticker"} label="Ticker" />
+        <LowerthirdModeCard value={"ad"} label="Solo Ad" />
+        <LowerthirdModeCard value={"adPool"} label="Ad Pool" />
+        <LowerthirdModeCard value={"matchPoll"} label="Match Poll" />
+      </Group>
     </Stack>
   )
 }
