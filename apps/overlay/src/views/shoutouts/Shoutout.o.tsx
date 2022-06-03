d7b202b0e78c698eec4fb964f3364f722200ce08
Jasper Concepcion <jasper@manok.dev>
2022-06-04 00:32:43 +0800

    feat: add shoutout image

--- apps/overlay/src/views/shoutouts/Shoutout.o.tsx
+++ apps/overlay/src/views/shoutouts/Shoutout.o.tsx
@@ -1,11 +1,10 @@
-import { Avatar, Group, Stack, Text } from "@mantine/core"
+import { Avatar, Box, Group, Stack, Text } from "@mantine/core"
 import { TwitterApiResultsItem } from "interface/utils"
 import { FC } from "react"
 import { useLive } from "utils/hooks"
 import useRoom from "../../hooks/useRoom.hook"
 import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
 import { QueryColor } from "../../utils/queryParams"
-import { Container } from "@mantine/core"
 import { decodeEntities } from "utils/general"
 
 const width = 750
@@ -21,12 +20,7 @@ const Shoutout = () => {
 
   const liveEntries = Object.entries(shoutouts)
   return (
-    <Stack
-      sx={{
-        width,
-        height: 1080,
-      }}
-    >
+    <Stack>
       {liveEntries.map(([id, tweet]) => (
         <Stack style={{ height: maxHeight }} justify="center">
           <ShoutoutItem tweet={tweet} key={id} />
@@ -41,52 +35,64 @@ interface ShoutoutItemProps {
 }
 const ShoutoutItem: FC<ShoutoutItemProps> = ({ tweet }) => {
   return (
-    <Container>
-      <Group>
-        <Group>
-          <Group>
-            <Avatar size="lg" src={tweet.user.profile_image_url} />
-            <Stack spacing={0}>
-              <Text
-                align="left"
-                sx={{
-                  fontSize: 30,
-                  lineHeight: 1,
-                  fontFamily: fontHeader,
-                  color: QueryColor["yellow"],
-                }}
-              >
-                {tweet.user.name}
-              </Text>
-
-              <Text
-                align="left"
-                color="dimmed"
-                sx={{
-                  lineHeight: 1,
-                  fontFamily: fontHeader,
-                  color: "#dddddd",
-                }}
-              >
-                @{tweet.user.username}
-              </Text>
-            </Stack>
+    <Group>
+      <Group ml={300} sx={{ position: "relative" }}>
+        <Box
+          sx={{
+            position: "absolute",
+            right: "calc(100% + 63px)",
+            height: 190,
+            width: 210,
+            borderRadius: 5,
+            backgroundImage: `url("${tweet.images[0]}")`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></Box>
+        <Group sx={{ width: 750 }}>
+          <Avatar size="lg" src={tweet.user.profile_image_url} />
+          <Stack spacing={0}>
             <Text
               align="left"
               sx={{
-                lineHeight: 1.1,
-                fontSize: 25,
-                wordBreak: "break-word",
-                fontFamily: fontContent,
-                color: QueryColor["white"],
+                fontSize: 30,
+                lineHeight: 1,
+                fontFamily: fontHeader,
+                color: QueryColor["yellow"],
               }}
             >
-              {decodeEntities(tweet.text)}
+              {tweet.user.name}
             </Text>
-          </Group>
+
+            <Text
+              align="left"
+              color="dimmed"
+              sx={{
+                lineHeight: 1,
+                fontFamily: fontHeader,
+                color: "#dddddd",
+              }}
+            >
+              @{tweet.user.username}
+            </Text>
+          </Stack>
+          <Text
+            align="left"
+            sx={{
+              lineHeight: 1.1,
+              fontSize: 25,
+              wordBreak: "break-word",
+              fontFamily: fontContent,
+              color: QueryColor["white"],
+            }}
+            lineClamp={3}
+          >
+            {decodeEntities(tweet.text)}
+          </Text>
         </Group>
       </Group>
-    </Container>
+    </Group>
   )
 }
 
