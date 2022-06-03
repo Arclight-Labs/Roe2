2017f97997054b2e9a3849e457c9660d3d036da6
Jasper Concepcion <jasper@manok.dev>
2022-06-03 23:42:31 +0800

    fix: single ad animation

--- apps/overlay/src/views/lowerthirds/AdSingle.o.tsx
+++ apps/overlay/src/views/lowerthirds/AdSingle.o.tsx
@@ -3,6 +3,7 @@ import { Text, Image, Group, Stack } from "@mantine/core"
 import useRoom from "../../hooks/useRoom.hook"
 import { FC } from "react"
 import { Ad } from "interface/ws"
+import { AnimatePresence, motion } from "framer-motion"
 interface AdItemProps {
   ad: Ad
 }
@@ -54,18 +55,32 @@ export const AdItem: FC<AdItemProps> = ({ ad }) => {
 const AdSingle = () => {
   useRoom()
   const { ad, adPool } = useLt()
-  const activeAd = adPool.ads.filter((currentAd) => {
+  const [activeAd] = adPool.ads.filter((currentAd) => {
     if (currentAd.id === ad) {
       return currentAd
     }
   })
 
   return (
-    <div>
-      {activeAd.map((ad) => {
-        return <AdItem ad={ad} key={ad.id} />
-      })}
-    </div>
+    <AnimatePresence>
+      {activeAd && (
+        <motion.div
+          key={activeAd.id}
+          exit={{ opacity: 0, y: -100 }}
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          style={{
+            height: "100%",
+            width: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+        >
+          <AdItem ad={activeAd} />
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
 
