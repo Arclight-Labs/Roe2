9f83e3e6c563f3cb20f00be2cfca279bd42f8e3d
Jasper <jasper@manok.dev>
2022-09-23 07:31:37 +0800

    fix(firebase-admin): use base64 credentials

--- packages/utils/firebase/firebase-admin.instance.ts
+++ packages/utils/firebase/firebase-admin.instance.ts
@@ -1,10 +1,14 @@
 import "dotenv/config"
-import { initializeApp } from "firebase-admin/app"
+import { cert, initializeApp } from "firebase-admin/app"
 import { getAuth as getAdminAuth } from "firebase-admin/auth"
 import { getFirestore } from "firebase-admin/firestore"
 import { getStorage as getAdminStorage } from "firebase-admin/storage"
 
-const app = initializeApp({ projectId: "roe2-prod" })
+const base64 = process.env.FIREBASE_CREDENTIALS_BASE64 || ""
+const credentials = JSON.parse(Buffer.from(base64, "base64").toString("utf8"))
+const app = initializeApp(
+  { credential: cert(credentials) } || { projectId: "roe2-prod" }
+)
 export const getDB = () => getFirestore(app)
 export const getAuth = () => getAdminAuth(app)
 export const getStorage = () => getAdminStorage(app)
