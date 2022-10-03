ce48f1a6c2afb618d2d50bcf06ec10ddf7590d57
Jasper <jasper@manok.dev>
2022-10-04 06:43:10 +0800

    fix(notif): fix notif not showing up

--- packages/utils/socket/events/Notif.emit.ts
+++ packages/utils/socket/events/Notif.emit.ts
@@ -2,5 +2,5 @@ import { Notif, SocketEvent } from "interface/ws"
 import { socket } from "../Socket.instance"
 
 export const notif: Notif = (accessToken) => (props) => {
-  socket.emit(SocketEvent.Error, props, accessToken)
+  socket.emit(SocketEvent.Notify, props, accessToken)
 }
