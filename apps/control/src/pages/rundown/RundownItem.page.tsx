6e51571d5e0f3cb7216961a4509dc23f4135484b
Jasper <jasper@manok.dev>
2022-11-06 00:56:55 +0800

    style: fix styling

--- apps/control/src/pages/rundown/RundownItem.page.tsx
+++ apps/control/src/pages/rundown/RundownItem.page.tsx
@@ -1,12 +1,7 @@
-import { Container } from "@mantine/core"
 import RundownContent from "../../ui/rundown/RundownItem.content"
 
 const RundownItemPage = () => {
-  return (
-    <Container sx={{ width: "100%" }}>
-      <RundownContent />
-    </Container>
-  )
+  return <RundownContent />
 }
 
 export default RundownItemPage
--- apps/control/src/ui/Navbar.ui.tsx
+++ apps/control/src/ui/Navbar.ui.tsx
@@ -9,7 +9,6 @@ import {
 import { useState } from "react"
 import { Link } from "react-router-dom"
 import routes from "./NavRoutes"
-import OBSConnection from "./obs/OBSConnection.ui"
 import RoomButton from "./room/RoomButton.ui"
 import UserButton from "./user/UserButton.ui"
 
@@ -136,7 +135,7 @@ export function NavbarSimple({ version }: Props) {
       </Navbar.Section>
 
       <Navbar.Section className={classes.footer}>
-        <OBSConnection variant="button" />
+        {/* <OBSConnection variant="button" /> */}
         <RoomButton />
         <UserButton />
       </Navbar.Section>
